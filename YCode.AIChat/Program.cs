07ecd6ced80bc3c7132a77fbb79d29f3465097a7
YCode <joestudy@petalmail.com>
2025-05-18 21:03:37 +0800

    Add project files.

---
+++ YCode.AIChat/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace YCode.AIChat
+{
+	internal class Program
+	{
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			var app = new Application();
+
+			var window = new Window();
+
+			window.Title = "YCode.AIChat";
+
+			window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+			app.Run(window);
+		}
+	}
+}
